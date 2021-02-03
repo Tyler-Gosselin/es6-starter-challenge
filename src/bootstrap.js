836f3c50eee3629a9f28994f712dfca1f694684b
Tyler <gosselin.code@gmail.com>
2021-02-03 09:06:27 -0700

    Initial Commit

---
+++ src/bootstrap.js
@@ -0,0 +1,5 @@
+import classChallenge, {greeting, sum} from './classChallenge'
+
+console.log(classChallenge())
+console.log(sum)
+console.log(greeting)
---
+++ src/classChallenge.js
@@ -0,0 +1,8 @@
+export default function () {
+    return '<div> classChallenge</div>'
+}
+
+export const greeting = 'Welcome'
+
+export const sum = [10, 20, 30].reduce((a, b) => a + b)
+console.log(sum); 
